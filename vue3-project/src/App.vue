4fcf5ff0f269a352cf3810d4ce408359d3009ea1
eunjuny <eunjuny@strato.co.kr>
2023-02-20 20:11:57 +0900

    zz

---
+++ vue3-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="name">{{ name }}</div>
+  <button 
+  class="btn btn-primary"
+  v-on:click="updateName">
+  Click
+  </button>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup(){
+    let name = ref("Kossie");
+    
+    // const greeting = () => {
+    //   return 'Hello, ' + name;
+      
+    // };
+    const updateName = () => {
+      name.value = 'Coder';
+    }
+
+    return {
+      name,
+      updateName
+    };
+  }
+}
+</script>
+
+<style>
+</style>
