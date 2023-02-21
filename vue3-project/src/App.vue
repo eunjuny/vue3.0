135351ea5400319af30f9e640f4b5e328337749d
eunjuny <eunjuny@strato.co.kr>
2023-02-21 20:21:01 +0900

    2.21

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="name">{{ name }}</div>
+  <div class="container">
+  <h2>To-Do List</h2>
+  <form 
+  @submit.prevent="onSubmit"
+  class="d-flex">
+    <div class="flex-grow-1 mr-2">
+    <input 
+    class="form-control"
+  type="text" 
+  v-model="todo"
+  placeholder="Type new to-do"
+  >
+</div>
+<div>
   <button 
   class="btn btn-primary"
-  v-on:click="updateName">
-  Click
+  type="submit"
+  @click="onSubmit">
+  Add
   </button>
+</div>
+  </form>
+  {{ todos }}
+</div>
 </template>
 
 <script>
@@ -12,23 +30,26 @@ import { ref } from 'vue';
 
 export default {
   setup(){
-    let name = ref("Kossie");
-    
-    // const greeting = () => {
-    //   return 'Hello, ' + name;
-      
-    // };
-    const updateName = () => {
-      name.value = 'Coder';
+    let todo = ref('');
+    const todos = ref([]);
+
+    const onSubmit = () => {
+      todos.value.push({
+        id: Date.now(),
+        subject : todo.value
+      })
     }
 
     return {
-      name,
-      updateName
+      todo,
+      todos,
+      onSubmit,
     };
   }
 }
 </script>
 
 <style>
+
 </style>
+
