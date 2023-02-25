882df69d656bc78742b436144b7073e05a76bd1a
eunjuny <eunjuny@strato.co.kr>
2023-02-26 01:31:20 +0900

    2.25(2)

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
+    <input
+      class="form-control"
+      type="text"
+      v-model="searchText"
+      placeholder="Search"
+    />
+    <hr />
     <TodoSimpleForm @add-todo="addTodo" />
-    
-    <div v-if="!todos.length">
-      추가된 Todo가 없습니다
+
+    <div v-if="!filteredTodos.length">
+      검색 결과가 없습니다.
     </div>
-    <TodoList 
-      :todos="todos" 
+    <TodoList
+      :todos="filteredTodos"
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
@@ -15,9 +22,10 @@
 </template>
 
 <script>
-import { ref } from 'vue';
-import TodoSimpleForm from './components/TodoSimpleForm.vue';
-import TodoList from './components/TodoList.vue';
+import { ref, computed } from "vue";
+import TodoSimpleForm from "./components/TodoSimpleForm.vue";
+import TodoList from "./components/TodoList.vue";
+import axios from 'axios';
 
 export default {
   components: {
@@ -27,8 +35,38 @@ export default {
   setup() {
     const todos = ref([]);
 
-    const addTodo = (todo) => {
-      todos.value.push(todo);
+    const getTodos = async () => {
+      try{
+      const res = await axios.get('http://localhost:3000/todos');
+        todos.value = res.data;
+      } catch (err){
+        console.log(err);
+      }
+    };
+
+    getTodos();
+
+    const addTodo = async (todo) => {
+      try{
+        const res = await axios.post('http://localhost:3000/todos',{
+          subject: todo.subject,
+          completed: todo.completed,
+        });
+        todos.value.push(res.data);
+      }catch(err){
+        console.log(err);
+      
+      }
+      // axios.post('http://localhost:3000/todos',{
+      //   subject: todo.subject,
+      //   completed: todo.completed,
+      // }).then(res => {
+      //   console.log(res);
+      //   todos.value.push(res.data);
+      // }).catch(err => {
+      //   console.log(err);
+      // })
+      
     };
 
     const deleteTodo = (index) => {
@@ -36,23 +74,36 @@ export default {
     };
 
     const toggleTodo = (index) => {
-      console.log(index)
-      todos.value[index].completed = !todos.value[index].completed
+      console.log(index);
+      todos.value[index].completed = !todos.value[index].completed;
     };
+
+    const searchText = ref("");
+    const filteredTodos = computed(() => {
+      if(searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      return todos.value;
+    })
 
     return {
       todos,
       addTodo,
       deleteTodo,
       toggleTodo,
+      searchText,
+      filteredTodos,
+      getTodos,
     };
-  }
-}
+  },
+};
 </script>
 
 <style>
-  .todo {
-    color: gray;
-    text-decoration: line-through;
-  }
+.todo {
+  color: gray;
+  text-decoration: line-through;
+}
 </style>
